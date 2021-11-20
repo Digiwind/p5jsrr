366644327b5bdcbabdb2e3ff7e31350ed68f188a
Digiwind <93961917+Digiwind@users.noreply.github.com>
2021-11-20 00:13:02 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,14 @@
-var video
+var song
 
 function preload() {
-  video = createVideo('rickrollvideo.mp4')
+  video = createSong('rickroll.mp3')
 }
 
 function setup () {
     createCanvas(windowWidth, windowHeight);
-    video.size(windowWidth, windowHeight);
 }
 
 function draw () {
     background(0)
-    video.play()
-    video.volume(1);
-    let img = video.get();
-        image(img, 0, 0); 
+    song.play()
 } 
