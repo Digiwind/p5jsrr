597ba66d602dd8023e62960a50da50290d387b6d
Digiwind <93961917+Digiwind@users.noreply.github.com>
2021-11-20 00:08:28 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,13 +6,13 @@ function preload() {
 
 function setup () {
     createCanvas(windowWidth, windowHeight);
-    video.play()
     video.size(windowWidth, windowHeight);
-    video.volume(1);
 }
 
 function draw () {
     background(0)
+    video.play()
+    video.volume(1);
     let img = video.get();
         image(img, 0, 0); 
 } 
